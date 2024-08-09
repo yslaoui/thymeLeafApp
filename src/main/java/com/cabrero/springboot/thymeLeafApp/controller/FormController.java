162261b6bd12aa0ae1fd4bfdd2e3126569244311
yslaoui <yslaouiyassine@gmail.com>
2024-08-09 15:00:04 +0100

    handling form data in controller using HttpServletRequest and Model

--- src/main/java/com/cabrero/springboot/thymeLeafApp/controller/FormController.java
+++ src/main/java/com/cabrero/springboot/thymeLeafApp/controller/FormController.java
@@ -1,6 +1,8 @@
 package com.cabrero.springboot.thymeLeafApp.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -13,6 +15,13 @@ public class FormController {
 
     @GetMapping("/processform")
     public String processTheForm() {
+        return "nameresult";
+    }
+
+    @GetMapping("/processformversiontwo")
+    public String shoutOutLoud(HttpServletRequest request, Model model) {
+        String name = request.getParameter("username").toUpperCase();
+        model.addAttribute("message", name);
         return "nameresult";
     }
 

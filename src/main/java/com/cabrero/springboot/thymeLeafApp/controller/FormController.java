046a1cabf2644ffe2c4c51157fe8b457cbb0d1ff
yslaoui <yslaouiyassine@gmail.com>
2024-08-12 11:20:26 +0100

    Adding dropdow list of countries stored in application.properties file

--- src/main/java/com/cabrero/springboot/thymeLeafApp/controller/FormController.java
+++ src/main/java/com/cabrero/springboot/thymeLeafApp/controller/FormController.java
@@ -3,6 +3,7 @@ package com.cabrero.springboot.thymeLeafApp.controller;
 import com.cabrero.springboot.thymeLeafApp.model.Student;
 import jakarta.servlet.http.HttpServletRequest;
 import org.apache.catalina.util.ToStringUtil;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.Banner;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -11,8 +12,12 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.List;
+
 @Controller
 public class FormController {
+    @Value("${countries}")
+    private List<String> countries;
 
     @GetMapping("/showform")
     public String showTheForm() {
@@ -40,6 +45,7 @@ public class FormController {
     @GetMapping("/studentForm")
     public String studentForm(Model model) {
         model.addAttribute("student", new Student());
+        model.addAttribute("countries", countries);
         return "studentform";
     }
 
